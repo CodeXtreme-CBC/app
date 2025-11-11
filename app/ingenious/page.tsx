060d6ac1a3717829bba4f64dbd3fac7026d5ae89
CedricMurairi <murairicedric@gmail.com>
2025-11-11 20:16:31 +0200

    under dev on app

--- app/ingenious/page.tsx
+++ app/ingenious/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function Ingenious() {
   return (
     <main className="flex h-[100dvh] w-full flex-col items-center justify-center bg-white text-black">
-      <div className="space-y-5 w-full px-[5%] md:w-[60%] mx-auto text-center"><h1 className="text-3xl font-semibold">Discover. Inspire. Nurture. Activate. Accelerate.</h1>
+      <div className="space-y-5 w-full px-[5%] md:w-[60%] mx-auto text-center"><h1 className="text-3xl font-semibold">Discover. Inspire. Activate. Nurture. Accelerate.</h1>
       <p>
         Ingenious is a 1-Year program we are designing to discover, inspire, nurture, activate and accelerate early tech talent and founders to build the next generation of world-class tech companies that will drive impact on the African continent.
       </p>
