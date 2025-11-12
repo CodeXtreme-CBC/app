66b262cb3fbcc040e237d8e313821d727662fa8f
CedricMurairi <murairicedric@gmail.com>
2025-11-12 09:24:59 +0200

    under dev on app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="space-y-5 flex flex-col h-[100dvh] w-full items-center justify-center py-32 px-16 bg-white text-black text-center">
-      <h1 className="text-3xl font-semibold">Under Development</h1>
+      <h1 className="text-2xlmd:text-3xl font-semibold">Under Development</h1>
       <p>Check back on February 1st</p>
     </main>
   );
